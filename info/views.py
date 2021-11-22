79498bdb455b87eb4678891c82e42ea6bb037478
malhar <malharlakdawala@gmail.com>
2021-11-22 10:44:03 +0530

    django

--- info/views.py
+++ info/views.py
@@ -5,14 +5,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def family(request,id):
+#     with open('data.json') as f:
+#         data = json.load(f)
+#     selected_family=None
+#     for family in data['families']:
+#         if family['id']==id:
+#             selected_family=family
+#     return render(request,'family.html',{'family':selected_family})
+
 def family(request,id):
     with open('data.json') as f:
         data = json.load(f)
-    selected_family=None
-    for family in data['families']:
-        if family['id']==id:
-            selected_family=family
-    return render(request,'family.html',{'family':selected_family})
+    selected_animal=[]
+    for animal in data['animals']:
+        if animal['family']==id:
+            selected_animal.append(animal)
+            print(selected_animal)
+    return render(request,'family.html',{'animal':selected_animal})
 
 
 def animal(request, id):
